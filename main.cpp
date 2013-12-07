d3cb278d2df8eb626b8a810bffb0d252561eeb45
Anmol <anmolbiswas@gmail.com>
2013-12-07 20:42:10 +0530

    final form

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "num_extract.hpp"
 
 int main (int argc , char **argv){
+    clock_t time = clock();
     Mat img = imread(argv[1],1);
     Num_Extract::InParams params;
     params.classes = 10;
@@ -14,6 +15,10 @@ int main (int argc , char **argv){
     params.train_samples = 4;
     Num_Extract Num1(params);
     //Num1.setParams(params);
-	Num1.run(img);
+    Num_Extract::TaskReturn marker;
+    marker = Num1.getInfo(img);
+    time = clock()-time;
+    float runtime = ((float)time)/CLOCKS_PER_SEC;
+    cout<<"run time "<< runtime << endl;
     return 0;
 }
